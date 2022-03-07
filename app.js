65f5ce7b2d00959ff5db8f1467cf67f1497635a1
Shea Cronin <shealcronin@gmail.com>
2022-03-07 16:21:11 -0500

    Set up app for deployment to Heroku

--- app.js
+++ app.js
@@ -7,13 +7,15 @@ var logger = require('morgan');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
+const compression = require('compression');
+const helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-const mongoDb = process.env.MONGODB_DEV;
+const mongoDb = process.env.MONGODB_URI || process.env.MONGODB_DEV;
 mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongo connection error'));
@@ -22,10 +24,12 @@ db.on('error', console.error.bind(console, 'mongo connection error'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
